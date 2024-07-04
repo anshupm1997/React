6e08f57117bf51f5d5cea240d77427dd8f8c59b9
anshupm1997 <anshupulami@gmail.com>
2024-07-04 08:23:30 +0545

    first commit

---
+++ healme/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import Navbar from './Navbar';
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <section></section>
+//     </>
+//   );
+// }
+
+// export default App;
+import React, {Component} from "react";
+
+
---
+++ healme/src/Navbar.js
@@ -0,0 +1,38 @@
+import './Navbar.css';
+import { useState } from 'react';
+
+function Navbar() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <nav>
+      <input type="checkbox" id="check" checked={isChecked} onChange={handleCheckboxChange} />
+      <label htmlFor="check" className="checkbtn">
+        <i className="fas fa-bars"></i>
+      </label>
+      <label className="logo">DesignX</label>
+      <ul>
+        <li><a className="active" href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Feedback</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <section></section>
+    </div>
+  );
+}
+
+export default App;
